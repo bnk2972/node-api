c829054643c9abc11359a41137be173dba00b09c
BNK 2972 <beer@livingmobile.me>
2021-09-25 00:59:31 +0700

    set response code

--- controllers/test-controller.js
+++ controllers/test-controller.js
@@ -1,7 +1,9 @@
 const { expressHandler } = require("@controllers/express-handler")
 
 async function test(request, response) {
-    response.json({ code: 200 })
+    const { menu_id } = request.body
+    if (!menu_id) return { code: 400 }
+    else return { code: 200 }
 }
 
 module.exports = {
