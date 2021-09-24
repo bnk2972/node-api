95fc3ee34eb22b575d51991b4e15a81c35b3ab79
BNK 2972 <beer@livingmobile.me>
2021-09-25 00:41:15 +0700

    disabled x-powered-by

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 
 const bodyParser = require('body-parser')
 
+app.disable('x-powered-by')
 app.use(bodyParser.json({ limit: '25mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '25mb' }))
 
